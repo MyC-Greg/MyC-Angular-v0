cd8f4ccf6d2db1f249d303238933476f34363398
MyC <nedelecgregoire@gmail.com>
2017-05-19 14:58:06 +0200

    add outer element click

---
+++ src/app/companions/companions-creation/companion-element/companion-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-companion-element',
+  templateUrl: './companion-element.component.html',
+  styleUrls: ['./companion-element.component.css']
+})
+export class CompanionElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/companions/companions-creation/companion-selection/companion-selection.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+
+import { Companion } from './../companion.model';
+import { CompanionsService } from './../companions.service';
+
+@Component({
+  selector: 'app-companion-selection',
+  host: {
+        '(document:click)': 'handleClick($event)',
+    },
+  templateUrl: './companion-selection.component.html',
+  styleUrls: ['./companion-selection.component.css']
+})
+export class CompanionSelectionComponent implements OnInit {
+  companions: Companion[];
+  filteredCompanions = [];
+  searchedCompanion;
+
+  constructor(private CompanionsService: CompanionsService,
+              private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    this.companions = this.CompanionsService.getCompanions();
+  }
+
+filter () {
+    if (this.searchedCompanion !== '') {
+          this.filteredCompanions = this.companions.filter(function(el){
+              return el.firstName.toLowerCase().indexOf(this.searchedCompanion.toLowerCase()) > -1 
+              || 
+              el.name.toLowerCase().indexOf(this.searchedCompanion.toLowerCase()) > -1;
+          }.bind(this));
+      }else{
+          this.filteredCompanions = [];
+      }
+  }
+
+  select(item){
+    this.searchedCompanion = item;
+    this.filteredCompanions = [];
+  }
+
+  handleClick(event){
+   let clickedComponent = event.target;
+   let inside = false;
+   do {
+       if (clickedComponent === this.elementRef.nativeElement) {
+           inside = true;
+       }
+      clickedComponent = clickedComponent.parentNode;
+   } while (clickedComponent);
+    if (!inside){
+        this.filteredCompanions = [];
+    }
+}
+
+}
